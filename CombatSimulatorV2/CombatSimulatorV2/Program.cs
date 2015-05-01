2e4cac85dd7a1ba3cb193e844f600b7e9366b279
Ryan Wilson <ryanmitchellwilson@gmail.com>
2015-05-01 15:46:14 -0600

    changed song and added game start screen

--- CombatSimulatorV2/CombatSimulatorV2/Program.cs
+++ CombatSimulatorV2/CombatSimulatorV2/Program.cs
@@ -11,17 +11,24 @@ namespace CombatSimulatorV2
     {
         static void Main(string[] args)
         {
+            //create a new object of the game
             Game game = new Game();
+            //play the game
             game.PlayGame();
 
             Console.ReadKey();
         }
     }
 
+    //create an abstract class that all players will have
     public abstract class Actor
     {
+        //set up properties
+        //name
         public string Name { get; set; }
+        //Hit Points
         public int HP { get; set; }
+        //read only propertiy just to check if the player is alive
         public bool IsAlive
         {
             get
@@ -30,36 +37,53 @@ namespace CombatSimulatorV2
             }
         }
 
+        //constructor to set the properties
         public Actor(string name, int hp)
         {
             this.Name = name;
             this.HP = hp;
         }
 
+        //a function that can be overwritten
         public virtual void Attack(Actor actor) { }
     }
 
+    //create a class for enemy inheriting from the actor class
     public class Enemy : Actor
     {
+        //build the constuctor for the enemy class
         public Enemy(string name, int hp)
             : base(name, hp) { }
 
+        //override the attack function
         public override void Attack(Actor actor)
         {
+            //create a random number generator
             Random rng = new Random();
+            //grab a number between 1 and 10
             int probabilityOfEnemyHit = rng.Next(1, 11);
 
+            //hit 80% of the time
             if (probabilityOfEnemyHit < 9)
             {
+                //remove health from the enemy
                 actor.HP -= rng.Next(5, 16);
-                Console.WriteLine("Hans hit you with all his might");
-                Thread.Sleep(750);
+                Console.WriteLine("Hans managed to land a hit somehow");
+                Thread.Sleep(1000);
+            }
+            //if hit misses
+            else
+            {
+                Console.WriteLine("Hans was no match for your skills");
+                Thread.Sleep(1000);
             }
         }
     }
 
+    //create a class for the player inheriting from the actor class
     public class Player : Actor
     {
+        //create an enum for the attack type
         public enum AttackType
         {
             AttackOne = 1,
@@ -67,9 +91,11 @@ namespace CombatSimulatorV2
             Heal
         }
 
+        //set the properties for the player
         public Player(string name, int hp)
             : base(name, hp) { }
 
+        //override the attack function
         public override void Attack(Actor actor)
         {
             Random rng = new Random();
@@ -77,33 +103,38 @@ namespace CombatSimulatorV2
             int probabilityOfAttack = 0;
             switch (attack)
             {
+                //for first attack type
                 case AttackType.AttackOne:
                     probabilityOfAttack = rng.Next(1, 11);
+                    //hits 70% of the time
                     if (probabilityOfAttack < 8)
                     {
                         actor.HP -= rng.Next(20, 36);
                         Console.WriteLine("You hit Hans square in the noggin");
-                        Thread.Sleep(750);
+                        Thread.Sleep(1000);
                     }
                     else
                     {
                         Console.WriteLine("Your kicking skills were not up to par");
-                        Thread.Sleep(750);
+                        Thread.Sleep(1000);
                     }
                     break;
+                    //for second attack type
                 case AttackType.AttackTwo:
                     actor.HP -= rng.Next(10, 16);
                     Console.WriteLine("Body Blow!");
-                    Thread.Sleep(750);
+                    Thread.Sleep(1000);
                     break;
+                    //to heal
                 case AttackType.Heal:
                     this.HP += rng.Next(10, 21);
                     Console.WriteLine("You became more BadAss");
-                    Thread.Sleep(750);
+                    Thread.Sleep(1000);
                     break;
             }
         }
 
+        //function to choose the attack
         private AttackType ChooseAttack()
         {
             string playerChoice = string.Empty;
@@ -113,16 +144,21 @@ namespace CombatSimulatorV2
                 playerChoice = Console.ReadLine();
 
                 int playerChoiceInt = 0;
+                //check if input is a number
                 bool isNumber = int.TryParse(playerChoice, out playerChoiceInt);
 
+                //check if the input is a number and not an empty string
                 if (isNumber && playerChoice != string.Empty)
                 {
                     switch (playerChoiceInt)
                     {
+                            //if player chooses 1
                         case 1:
                             return AttackType.AttackOne;
+                        //if player chooses 2
                         case 2:
                             return AttackType.AttackTwo;
+                        //if player chooses 3
                         case 3:
                             return AttackType.Heal;
                     }
@@ -136,22 +172,28 @@ namespace CombatSimulatorV2
         public Player Player { get; set; }
         public Enemy Enemy { get; set; }
 
+        //set the player and the enemy
         public Game()
         {
-            Player = new Player("John McClain", 100);
+            Player = new Player("John McClane", 100);
             Enemy = new Enemy("Hans Gruber", 200);
         }
 
+        //display the game
         public void DisplayCombatInfo()
         {
-            Console.WriteLine("John McClain: {0}        Hans Gruber: {1}", this.Player.HP, this.Enemy.HP);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("{0}: {1}        {2}: {3}\n\n",this.Player.Name, this.Player.HP, this.Enemy.Name, this.Enemy.HP);
             Console.WriteLine("1) Kick");
             Console.WriteLine("2) Punch");
             Console.WriteLine("3) Shake it off");
         }
 
+        //play the game
         public void PlayGame()
         {
+            StartGame();
+            //while the player and the enemy are alive
             while (this.Player.IsAlive && this.Enemy.IsAlive)
             {
                 DisplayCombatInfo();
@@ -160,192 +202,52 @@ namespace CombatSimulatorV2
 
                 Console.Clear();
             }
+            //if player won
             if (this.Player.IsAlive)
             {
                 Console.WriteLine("Yippy Kai-Ay Motha Fucka!");
-                Console.Beep(659, 125);
-                Console.Beep(659, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(167);
-                Console.Beep(523, 125);
-                Console.Beep(659, 125);
-                Thread.Sleep(125);
-                Console.Beep(784, 125);
-                Thread.Sleep(375);
-                Console.Beep(392, 125);
-                Thread.Sleep(375);
-                Console.Beep(523, 125);
-                Thread.Sleep(250);
-                Console.Beep(392, 125);
-                Thread.Sleep(250);
-                Console.Beep(330, 125);
-                Thread.Sleep(250);
-                Console.Beep(440, 125);
-                Thread.Sleep(125);
-                Console.Beep(494, 125);
-                Thread.Sleep(125);
-                Console.Beep(466, 125);
-                Thread.Sleep(42);
-                Console.Beep(440, 125);
-                Thread.Sleep(125);
-                Console.Beep(392, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(125);
-                Console.Beep(784, 125);
-                Thread.Sleep(125);
-                Console.Beep(880, 125);
-                Thread.Sleep(125);
-                Console.Beep(698, 125);
-                Console.Beep(784, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(125);
-                Console.Beep(523, 125);
-                Thread.Sleep(125);
-                Console.Beep(587, 125);
-                Console.Beep(494, 125);
-                Thread.Sleep(125);
-                Console.Beep(523, 125);
-                Thread.Sleep(250);
-                Console.Beep(392, 125);
-                Thread.Sleep(250);
-                Console.Beep(330, 125);
-                Thread.Sleep(250);
-                Console.Beep(440, 125);
-                Thread.Sleep(125);
-                Console.Beep(494, 125);
-                Thread.Sleep(125);
-                Console.Beep(466, 125);
-                Thread.Sleep(42);
-                Console.Beep(440, 125);
-                Thread.Sleep(125);
-                Console.Beep(392, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(125);
-                Console.Beep(784, 125);
-                Thread.Sleep(125);
-                Console.Beep(880, 125);
-                Thread.Sleep(125);
-                Console.Beep(698, 125);
-                Console.Beep(784, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(125);
-                Console.Beep(523, 125);
-                Thread.Sleep(125);
-                Console.Beep(587, 125);
-                Console.Beep(494, 125);
-                Thread.Sleep(375);
-                Console.Beep(784, 125);
-                Console.Beep(740, 125);
-                Console.Beep(698, 125);
-                Thread.Sleep(42);
-                Console.Beep(622, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(167);
-                Console.Beep(415, 125);
-                Console.Beep(440, 125);
-                Console.Beep(523, 125);
-                Thread.Sleep(125);
-                Console.Beep(440, 125);
-                Console.Beep(523, 125);
-                Console.Beep(587, 125);
-                Thread.Sleep(250);
-                Console.Beep(784, 125);
-                Console.Beep(740, 125);
-                Console.Beep(698, 125);
-                Thread.Sleep(42);
-                Console.Beep(622, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(167);
-                Console.Beep(698, 125);
-                Thread.Sleep(125);
-                Console.Beep(698, 125);
-                Console.Beep(698, 125);
-                Thread.Sleep(625);
-                Console.Beep(784, 125);
-                Console.Beep(740, 125);
-                Console.Beep(698, 125);
-                Thread.Sleep(42);
-                Console.Beep(622, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(167);
-                Console.Beep(415, 125);
-                Console.Beep(440, 125);
-                Console.Beep(523, 125);
-                Thread.Sleep(125);
-                Console.Beep(440, 125);
-                Console.Beep(523, 125);
-                Console.Beep(587, 125);
-                Thread.Sleep(250);
-                Console.Beep(622, 125);
-                Thread.Sleep(250);
-                Console.Beep(587, 125);
-                Thread.Sleep(250);
-                Console.Beep(523, 125);
-                Thread.Sleep(1125);
-                Console.Beep(784, 125);
-                Console.Beep(740, 125);
-                Console.Beep(698, 125);
-                Thread.Sleep(42);
-                Console.Beep(622, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(167);
-                Console.Beep(415, 125);
-                Console.Beep(440, 125);
-                Console.Beep(523, 125);
-                Thread.Sleep(125);
-                Console.Beep(440, 125);
-                Console.Beep(523, 125);
-                Console.Beep(587, 125);
-                Thread.Sleep(250);
-                Console.Beep(784, 125);
-                Console.Beep(740, 125);
-                Console.Beep(698, 125);
-                Thread.Sleep(42);
-                Console.Beep(622, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(167);
-                Console.Beep(698, 125);
-                Thread.Sleep(125);
-                Console.Beep(698, 125);
-                Console.Beep(698, 125);
-                Thread.Sleep(625);
-                Console.Beep(784, 125);
-                Console.Beep(740, 125);
-                Console.Beep(698, 125);
-                Thread.Sleep(42);
-                Console.Beep(622, 125);
-                Thread.Sleep(125);
-                Console.Beep(659, 125);
-                Thread.Sleep(167);
-                Console.Beep(415, 125);
-                Console.Beep(440, 125);
-                Console.Beep(523, 125);
-                Thread.Sleep(125);
-                Console.Beep(440, 125);
-                Console.Beep(523, 125);
-                Console.Beep(587, 125);
-                Thread.Sleep(250);
-                Console.Beep(622, 125);
-                Thread.Sleep(250);
-                Console.Beep(587, 125);
-                Thread.Sleep(250);
-                Console.Beep(523, 125);
-                Thread.Sleep(625);
+                WinSong();
             }
+            //if player lost
             else
             {
-                Console.WriteLine("You Lose!");
+                Console.WriteLine("You Died Hard!");
             }
+        }
+
+        //song for winning
+        public void WinSong()
+        {
+            Console.Beep(587, 125);
+            Console.Beep(587, 125);
+            Console.Beep(587, 125);
+            Console.Beep(587, 500);
+            Console.Beep(466, 500);
+            Console.Beep(523, 500);
+            Console.Beep(587, 250);
+            Console.Beep(523, 250);
+            Console.Beep(587, 500);
+        }
+
+        //display for the start of the game
+        public void StartGame()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(@"▓█████▄  ██▓▓█████     ██░ ██  ▄▄▄       ██▀███  ▓█████▄  ▐██▌ ");
+            Console.WriteLine(@"▒██▀ ██▌▓██▒▓█   ▀    ▓██░ ██▒▒████▄    ▓██ ▒ ██▒▒██▀ ██▌ ▐██▌ ");
+            Console.WriteLine(@"░██   █▌▒██▒▒███      ▒██▀▀██░▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌ ▐██▌ ");
+            Console.WriteLine(@"░▓█▄   ▌░██░▒▓█  ▄    ░▓█ ░██ ░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌ ▓██▒ ");
+            Console.WriteLine(@"░▒████▓ ░██░░▒████▒   ░▓█▒░██▓ ▓█   ▓██▒░██▓ ▒██▒░▒████▓  ▒▄▄  ");
+            Console.WriteLine(@" ▒▒▓  ▒ ░▓  ░░ ▒░ ░    ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒  ░▀▀▒ ");
+            Console.WriteLine(@" ░ ▒  ▒  ▒ ░ ░ ░  ░    ▒ ░▒░ ░  ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒  ░  ░ ");
+            Console.WriteLine(@" ░ ░  ░  ▒ ░   ░       ░  ░░ ░  ░   ▒     ░░   ░  ░ ░  ░     ░ ");
+            Console.WriteLine(@"   ░     ░     ░  ░    ░  ░  ░      ░  ░   ░        ░     ░    ");
+            Console.WriteLine(@" ░                                                ░            ");
+            Console.WriteLine("\nPress Any Key To Continue:");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
